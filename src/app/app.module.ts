79dc787fb5cbdbe448e725b9d83e60b814c2b309
antoine nguyen hong duc <antoine.nguyen@makesense.org>
2017-08-06 21:08:04 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { MdButtonModule, MdCheckboxModule, MdMenuModule, MdToolbarModule, MdIconModule, MdCardModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule, BrowserAnimationsModule,
+    MdButtonModule, MdCheckboxModule, MdMenuModule, MdToolbarModule, MdIconModule, MdCardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
